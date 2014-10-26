f8d0a71e0c7391d87059d5a4a78aa1ed218171eb
saipramod <supadyay@hawk.iit.edu>
2014-10-26 01:28:25 -0500

    adding deployment descriptor

--- GlassFishDeployment/deployglassfish.sh
+++ GlassFishDeployment/deployglassfish.sh
@@ -8,9 +8,6 @@
 sudo apt-get -y update
 sudo apt-get -y install python-software-properties apache2 wget curl git unzip
 service apache2 stop
-#sudo add-apt-repository -y ppa:webupd8team/java
-#sudo apt-get -y update
-#sudo apt-get -y install oracle-java7-installer
 
 sudo apt-get -y install openjdk-7-jre
 sudo apt-get -y install openjdk-7-jdk
@@ -22,8 +19,15 @@ wget download.java.net/glassfish/4.0/release/glassfish-4.0.zip
 sudo unzip glassfish-4.0.zip -d /opt
 export PATH=/opt/glassfish4/bin:$PATH
 sudo chmod 777 -R /opt/*
+
+/opt/glassfish4/glassfish/domains/domain1/config/domain.xml
+
+sudo cat /opt/glassfish4/glassfish/domains/domain1/config/domain.xml | sed 's/"8080"/"80"/' > temp
+cp temp /opt/glassfish4/glassfish/domains/domain1/config/domain.xml
 asadmin start-domain
+sudo rm -r Amazons3upload
 #cd ~
 git clone https://github.com/saipramod/Amazons3upload.git
 #wget https://github.com/saipramod/Amazons3upload/blob/master/target/ITMD544ASS3-1.0-SNAPSHOT.war
-asadmin deploy /home/ubuntu/Amazons3upload/target/ITMD544ASS3-1.0-SNAPSHOT.war
+asadmin deploy --force=true /home/ubuntu/Amazons3upload/target/ITMD544ASS3-1.0-SNAPSHOT.war
+
